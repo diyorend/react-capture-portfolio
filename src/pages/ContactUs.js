0b4a3b204428c9bfca376acab7f6308110393913
Diyor <diyorend@gmail.com>
2022-05-02 16:32:31 +0500

    awful-contact-page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,67 @@
 import React from "react";
 //Animation
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
+import { About, Hide } from "../styles";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      ContactUs
-    </motion.div>
+      <Hide>
+        <Title variants={titleAnim}>Get in touch.</Title>
+      </Hide>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send Us a Massege</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send an Email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Social Media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(About)`
+  display: block;
+  background: #eee;
+  color: #353535;
+`;
+const Title = styled(motion.h2)`
+  color: #000;
+  margin-bottom: 3rem;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 1rem;
+    font-size: 3rem;
+  }
+`;
 
 export default ContactUs;
